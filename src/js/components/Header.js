ea93a411444f07b297902e50e66c59c97d9b97a4
mirekzhou <mirek.zhou@gmail.com>
2016-08-30 23:18:45 +0800

    header

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -4,38 +4,72 @@
 	}
 	
 	Header.prototype.initDom = function () {
-		var temp =	'<div class="header">' +
-						'<div class="wrapper">' +
-							'<div class="row1">' +
-								'<div class="language">' +
-									'语言' +
-								'</div>' +
+		var temp;
 
-								'<div class="bzzx top-item">' +
-									'设为首页' +
-								'</div>' +
+		this.switch = new Switch({id: 'money-switch'});
 
-								'<div class="bzzx top-item">' +
-									'帮助中心' +
-								'</div>' +
+		temp =	'<div class="header">' +
+					'<div class="wrapper">' +
+						'<div class="row1">' +
+							'<div class="language">' +
+								'语言' +
+							'</div>' +
 
-								'<div class="bzzx top-item">' +
-									'我的收藏' +
-								'</div>' +
+							'<div class="bzzx top-item">' +
+								'设为首页' +
+							'</div>' +
 
-								'<div class="money-actions">' +
-								 	'<div class="wrapper">' +
-										'<div class="cz action"><span>充值</span></div>' +
-										'<div class="tk action"><span>提款</span></div>' +
-										'<div class="zz action"><span>转账</span></div>' +
-									'</div>' +
+							'<div class="bzzx top-item">' +
+								'帮助中心' +
+							'</div>' +
+
+							'<div class="bzzx top-item">' +
+								'我的收藏' +
+							'</div>' +
+
+							'<div class="money-actions">' +
+							 	'<div class="wrapper">' +
+									'<div class="cz action"><span>充值</span></div>' +
+									'<div class="tk action"><span>提款</span></div>' +
+									'<div class="zz action"><span>转账</span></div>' +
 								'</div>' +
 							'</div>' +
 
-							'<div class="row2">' +
+							'<div class="balance">' +
+								'<div class="balance-item title">余额</div>' +
+								'<div class="balance-item money-icon">¥</div>' +
+								'<div class="balance-item balance-value">10000000.00</div>' +
+								this.switch.getDom() +
+							'</div>' +
+
+							'<div class="grzx">' +
+								'<div class="title">' +
+									'个人中心' +
+								'</div>' +
+
+								'<div class="message">' +
+									'<img class="message-img" src="../img/message.png">' +
+									'<div class="dot">1</div>' +
+								'</div>' +
+							'</div>' +
+
+							'<div class="time">' +
+								'<div class="time-zone">' +
+									'GMT +8' +
+								'</div>' +
+
+								'<div class="time-value">' +
+									'12:15:30' +
+								'</div>' +
+
+								'<div class="clear"></div>' +
 							'</div>' +
 						'</div>' +
-					'</div>';
+
+						'<div class="row2">' +
+						'</div>' +
+					'</div>' +
+				'</div>';
 		
 		this.el  = temp;
 	};
@@ -51,6 +85,9 @@
 	};
 
 	Header.prototype.bindEvents = function () {
+		this.zone = $('.header');
+
+		this.switch.bindEvents();
 	};
 
 	window.Header = Header;
---
+++ src/js/plugins/Swithch.js
@@ -0,0 +1,32 @@
+(function () {
+	function Switch (opt) {
+		this.opt = opt;
+		this.id  = opt.id;
+		this.initDom();
+	}
+	
+	Switch.prototype.initDom = function () {
+		var temp =	'<div class="switch" id="' + this.id + '">' +
+						'<div class="switch-ball">' +
+						'</div>' +
+					'</div>';
+		
+		this.el  = temp;
+	};
+
+	Switch.prototype.getDom = function () {
+		return this.el;
+	};
+
+	Switch.prototype.bindEvents = function () {
+		var that = this;
+
+		this.zone = $('#' + this.id);
+
+		this.zone.click(function () {
+			that.zone.toggleClass('switch-selected');
+		});
+	};
+
+	window.Switch = Switch;
+}());
