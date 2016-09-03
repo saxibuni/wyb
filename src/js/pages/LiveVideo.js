a39d38e53d51c8e7e4e6326f711729c04861006b
mirekzhou <mirek.zhou@gmail.com>
2016-09-03 16:34:15 +0800

    live video

--- src/js/pages/LiveVideo.js
+++ src/js/pages/LiveVideo.js
@@ -6,7 +6,41 @@
 	LiveVideo.prototype.initDom = function () {
 		var temp =	'<div class="page live-video">' +
 						'<div class="wrapper">' +
-							'真人视讯' +
+							'<div class="title">' +
+							'</div>' +
+
+							'<div class="content">' +
+								'<table cellspacing="10" cellpadding="10">' +
+									'<tbody>' +
+										'<tr>' +
+											'<td colspan="2">' +
+												this.createItem() +
+											'</td>' +
+											'<td>' +
+												this.createItem() +
+											'</td>' +
+											'<td>' +
+												this.createItem() +
+											'</td>' +
+										'</tr>' +
+
+										'<tr>' +
+											'<td>' +
+												this.createItem() +
+											'</td>' +
+											'<td>' +
+												this.createItem() +
+											'</td>' +
+											'<td>' +
+												this.createItem() +
+											'</td>' +
+											'<td>' +
+												this.createItem() +
+											'</td>' +
+										'</tr>' +
+									'</tbody>' +
+								'</table>' +
+							'</div>' +
 						'</div>' +
 					'</div>';
 		
@@ -15,6 +49,29 @@
 
 	LiveVideo.prototype.getDom = function () {
 		return this.el;
+	};
+
+	LiveVideo.prototype.createItem = function () {
+		var temp =	'<div class="item">' +
+						'<div class="zone1">' +
+							'<div class="logo">' +
+								'<img src="../img/bg-eentertainment.jpg">' +
+							'</div>' +
+						'</div>' +
+
+						'<div class="zone2">' +
+							'<p class="title">' +
+								'AG国际厅，实时显示赢家排行胜率，百家乐，龙虎，轮盘...' +
+							'</p>' +
+
+							'<div class="quantity">' +
+								'<img class="message-img" src="../img/zh-lang.png">' +
+								'<span>2022</span>' +
+							'</div>' +
+						'</div>' +
+					'</div>';
+
+		return temp;
 	};
 
 	LiveVideo.prototype.show = function () {
