29c5db9c70c033289efd79879d3625032288d744
mirekzhou <mirek.zhou@gmail.com>
2016-09-09 17:47:15 +0800

    ~~

--- src/js/pages/SportsCompetition.js
+++ src/js/pages/SportsCompetition.js
@@ -6,7 +6,19 @@
 	SportsCompetition.prototype.initDom = function () {
 		var temp =	'<div class="page sports-competition">' +
 						'<div class="wrapper">' +
-							'体育竞技' +
+							'<div class="row1">' +
+								'<img src="../img/sports-competition-img1.png">' +
+							'</div>' +
+
+							'<div class="row2">' +
+								'<div class="sbty">' +
+									'<img src="../img/sports-competition-img2.png">' +
+								'</div>' +
+
+								'<div class="threedty">' +
+									'<img src="../img/sports-competition-img3.png">' +
+								'</div>' +
+							'</div>' +
 						'</div>' +
 					'</div>';
 		
@@ -27,6 +39,14 @@
 
 	SportsCompetition.prototype.bindEvents = function () {
 		this.zone = $('.sports-competition');
+
+		this.zone.find('.sbty').click(function () {
+			window.open('http://www.baidu.com');
+		});
+
+		this.zone.find('threedty').click(function () {
+			window.open('http://www.baidu.com');
+		});
 	};
 
 	window.SportsCompetition = SportsCompetition;
