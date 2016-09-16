a694e20f647f11e7306af38f98e5bd2d126f58c7
mirekzhou <mirek.zhou@gmail.com>
2016-09-16 10:33:43 +0800

    ~~

--- src/js/components/TopUp.js
+++ src/js/components/TopUp.js
@@ -14,8 +14,34 @@
 			height: 42
 		});
 
+		this.button2 = new Button({
+			id: 'topup-button2',
+			name: '立即充值',
+			width: 128,
+			height: 42
+		});
+
+		this.button3 = new Button({
+			id: 'topup-button3',
+			name: '提交订单',
+			width: 128,
+			height: 42
+		});
+
 		this.topupInput = new Input({
 			id: 'topup-input',
+			width: 200,
+			height: 30
+		});
+
+		this.topupInput2 = new Input({
+			id: 'topup-input2',
+			width: 200,
+			height: 30
+		});
+
+		this.topupInput3 = new Input({
+			id: 'topup-input3',
 			width: 200,
 			height: 30
 		});
@@ -23,77 +49,115 @@
 		temp = 		'<div class="top-up grzx-money-action">' +
 						'<div class="row1">' +
 							'<ul>' +
-								'<li class="selected">' +
+								'<li data-value="fast-topup" class="selected">' +
 									'<span>快捷支付</span>' +
 								'</li>' +
 
-								'<li>' +
+								'<li data-value="superfast-topup">' +
 									'<span>急速网银转账</span>' +
 								'</li>' +
 
-								'<li>' +
+								'<li data-value="bank-topup">' +
 									'<span>银行转账</span>' +
 								'</li>' +
 							'</ul>' +
 						'</div>' +
 
-						'<div class="row2">' +
-							'<div class="title">' +
-								'请选择充值银行：' +
+						'<div class="content">' +
+							'<div class="content-item fast-topup">' +
+								'<div class="row4">' +
+									'<div class="text">充值金额</div>' +
+									this.topupInput.getDom() +
+									'<div class="text unit">元</div>' +
+									'<div class="input-notice">' +
+										'充值额度限定： 最低2.00元，最高45000.00元' +
+									'</div>' +
+								'</div>' +
+
+								this.createBankCards() +
+
+								'<div class="row5">' +
+									this.button.getDom() +
+								'</div>' +
 							'</div>' +
 
-							'<ul>' +
-								'<li data-value="zggsyh">' +
-									'<input type="radio" name="bank" />' +
-									'<img style="top: -32px;" src="../img/bankLogo.jpg">' +
-								'</li>' +
-								'<li data-value="zgjsyh">' +
-									'<input type="radio" name="bank" />' +								
-									'<img style="top: -130px;" src="../img/bankLogo.jpg">' +
-								'</li>' +
-								'<li data-value="jtyh">' +
-									'<input type="radio" name="bank" />' +
-									'<img style="top: -165px;" src="../img/bankLogo.jpg">' +
-								'</li>' +
-								'<li data-value="zgyh">' +
-									'<input type="radio" name="bank" />' +
-									'<img style="top: -98px;" src="../img/bankLogo.jpg">' +
-								'</li>' +
-								'<li data-value="zgnyyh">' +
-									'<input type="radio" name="bank" />' +
-									'<img style="top: -67px;" src="../img/bankLogo.jpg">' +
-								'</li>' +
-								'<li data-value="zsyh">' +
-									'<input type="radio" name="bank" />' +
-									'<img style="top: -428px;" src="../img/bankLogo.jpg">' +
-								'</li>' +
-								'<li data-value="pfyh">' +
-									'<input type="radio" name="bank" />' +
-									'<img style="top: -495px;" src="../img/bankLogo.jpg">' +
-								'</li>' +
-								'<li data-value="zgyzcxyh">' +
-									'<input type="radio" name="bank" />' +
-									'<img style="top: -195px;" src="../img/bankLogo.jpg">' +
-								'</li>' +
-							'</ul>' +
-						'</div>' +
+							'<div class="content-item superfast-topup">' +
+								
+								this.createBankCards() +
 
-						'<div class="row3">' +
-							'<span>更多网银</span>' +
-							'<img class="message-img" src="../img/sxl.png">' +
-						'</div>' +
+								'<div class="row4">' +
+									'<div class="text">充值金额</div>' +
+									this.topupInput2.getDom() +
+									'<div class="text unit">元</div>' +
+									'<div class="input-notice">' +
+										'充值额度限定： 最低2.00元，最高45000.00元' +
+									'</div>' +
+								'</div>' +
 
-						'<div class="row4">' +
-							'<div class="text">充值金额</div>' +
-							this.topupInput.getDom() +
-							'<div class="text unit">元</div>' +
-							'<div class="input-notice">' +
-								'充值额度限定： 最低2.00元，最高45000.00元' +
+								'<div class="row5">' +
+									this.button2.getDom() +
+								'</div>' +
 							'</div>' +
-						'</div>' +
 
-						'<div class="row5">' +
-							this.button.getDom() +
+							'<div class="content-item bank-topup">' +
+								
+								this.createBankCards() +
+
+								'<table>' +
+									'<tbody>' +
+										'<tr>' +
+											'<td class="bank-name">收款银行</td>' +
+											'<td>中国工商银行</td>' +
+										'</tr>' +
+										'<tr>' +
+											'<td class="user-name">账户名</td>' +
+											'<td>刘文慧</td>' +
+										'</tr>' +
+										'<tr>' +
+											'<td class="account">收款账号</td>' +
+											'<td>collowzuez99@sina.com</td>' +
+										'</tr>' +
+									'</tbody>' +
+								'</table>' +
+
+								'<div class="deposit-method">' +
+									'<span class="title">存款方式</span>' +
+									'<ul>' +
+										'<li>' +
+											'<input name="deposit-method" type="radio" id="topup-wyzz" checked="checked">' +
+											'<label for="topup-wyzz">网银转账</label>' +
+										'</li>' +
+										'<li>' +
+											'<input name="deposit-method" type="radio" id="topup-atm-autocounter">' +
+											'<label for="topup-atm-autocounter">ATM自动柜员机</label>' +
+										'</li>' +
+										'<li>' +
+											'<input name="deposit-method" type="radio" id="topup-atm-cash">' +
+											'<label for="topup-atm-cash">ATM现金入款</label>' +
+										'</li>' +
+										'<li>' +
+											'<input name="deposit-method" type="radio" id="topup-bank-counter">' +
+											'<label for="topup-bank-counter">银行柜台</label>' +
+										'</li>' +
+										'<li>' +
+											'<input name="deposit-method" type="radio" id="topup-mobile-bank">' +
+											'<label for="topup-mobile-bank">手机银行</label>' +
+										'</li>' +
+									'</ul>' +
+
+								'<div class="row4">' +
+									'<div class="text">充值金额</div>' +
+									this.topupInput3.getDom() +
+									'<div class="text unit">元</div>' +
+									'<div class="input-notice">' +
+										'充值额度限定： 最低2.00元，最高45000.00元' +
+									'</div>' +
+								'</div>' +
+
+								'<div class="row5">' +
+									this.button3.getDom() +
+								'</div>' +
+							'</div>' +
 						'</div>' +
 					'</div>';
 
@@ -102,6 +166,56 @@
 
 	TopUp.prototype.getCardPosition = function(name) {
 
+	};
+
+	TopUp.prototype.createBankCards = function() {
+		var temp = '<div class="bank-cards">' +
+						'<div class="title">' +
+							'请选择充值银行：' +
+						'</div>' +
+
+						'<ul>' +
+							'<li data-value="zggsyh">' +
+								'<input type="radio" name="bank" />' +
+								'<img style="top: -32px;" src="../img/bankLogo.jpg">' +
+							'</li>' +
+							'<li data-value="zgjsyh">' +
+								'<input type="radio" name="bank" />' +								
+								'<img style="top: -130px;" src="../img/bankLogo.jpg">' +
+							'</li>' +
+							'<li data-value="jtyh">' +
+								'<input type="radio" name="bank" />' +
+								'<img style="top: -165px;" src="../img/bankLogo.jpg">' +
+							'</li>' +
+							'<li data-value="zgyh">' +
+								'<input type="radio" name="bank" />' +
+								'<img style="top: -98px;" src="../img/bankLogo.jpg">' +
+							'</li>' +
+							'<li data-value="zgnyyh">' +
+								'<input type="radio" name="bank" />' +
+								'<img style="top: -67px;" src="../img/bankLogo.jpg">' +
+							'</li>' +
+							'<li data-value="zsyh">' +
+								'<input type="radio" name="bank" />' +
+								'<img style="top: -428px;" src="../img/bankLogo.jpg">' +
+							'</li>' +
+							'<li data-value="pfyh">' +
+								'<input type="radio" name="bank" />' +
+								'<img style="top: -495px;" src="../img/bankLogo.jpg">' +
+							'</li>' +
+							'<li data-value="zgyzcxyh">' +
+								'<input type="radio" name="bank" />' +
+								'<img style="top: -195px;" src="../img/bankLogo.jpg">' +
+							'</li>' +
+						'</ul>' +
+					'</div>' +
+
+					'<div class="more-bank-cards">' +
+						'<span>更多网银</span>' +
+						'<img class="message-img" src="../img/sxl.png">' +
+					'</div>';
+
+		return temp;
 	};
 
 	TopUp.prototype.getDom = function() {
@@ -118,25 +232,37 @@
 
 	TopUp.prototype.bindEvents = function() {
 		var row1Ul;
-		this.zone = $('.top-up');
+		var bankCardsUl;
+		var content;
+		var contentName;
 
-		row1Ul = this.zone.find('.row1 ul');
-		row2Ul = this.zone.find('.row2 ul');
+		this.zone    = $('.top-up');
+		content      = this.zone.find('.content');
+		row1Ul       = this.zone.find('.row1 ul');
+		bankCardsUl  = this.zone.find('.bank-cards ul');
 
 		row1Ul.delegate('li', 'click', function () {
 			row1Ul.find('li').removeClass('selected');
 			$(this).addClass('selected');
+
+			contentName = $(this).attr('data-value');
+			content.children('.content-item').hide();
+			content.children('.' + contentName).show();
 		});
 
-		row2Ul.delegate('li','click',function(){
-			row2Ul.find('li').removeClass('selected');
+		bankCardsUl.delegate('li','click',function(){
+			bankCardsUl.find('li').removeClass('selected');
 			$(this).addClass('selected');
-			row2Ul.find('input[type="radio"]').attr('checked',false);
+			bankCardsUl.find('input[type="radio"]').attr('checked',false);
 			$(this).find('input[type="radio"]').attr('checked',true);
 		});
 
 		this.button.bindEvents();
+		this.button2.bindEvents();
+		this.button3.bindEvents();
 		this.topupInput.bindEvents();
+		this.topupInput2.bindEvents();
+		this.topupInput3.bindEvents();
 	};
 
 	window.TopUp = TopUp;
