c51ea24d3921f5fa996aae8799a2a7d1660e4dc9
mirek <mirek@eveb.com>
2016-10-14 13:54:03 +0800

    ~~

--- src/js/plugins/Suspension.js
+++ src/js/plugins/Suspension.js
@@ -14,12 +14,15 @@
                         '</li>' +
                         '<li>' +
                             '<div class="icon phone-icon"></div>' +
+                            '<div class="slipper">+6391578888</div>' +
                         '</li>' +
                         '<li>' +
                             '<div class="icon qq-icon"></div>' +
+                            '<div class="slipper">24小时在线客服</div>' +
                         '</li>' +
                         '<li>' +
                             '<div class="icon wechat-icon"></div>' +
+                            '<div class="slipper">伟易博</div>' +
                         '</li>' +
                         '<li>' +
                             '<div class="icon compass-icon"></div>' +
