d9b3f58f2783de882ca613a2ba2b86ce5e646d17
mirek <mirek@eveb.com>
2016-10-27 11:12:54 +0800

    ~~

--- src/js/components/SecurityCenter.js
+++ src/js/components/SecurityCenter.js
@@ -1,5 +1,5 @@
-$(function(){
-	function SecurityCenter(opt){
+$(function() {
+	function SecurityCenter(opt) {
 		this.opt = opt;
 		this.initDom();
 	}
@@ -10,39 +10,40 @@ $(function(){
 		temp = '<div class="security-center zhsz-info-action">' +
 					'<div class="wrapper">' +
 						'<div>' +
-							'<div class="status pass"></div>' +
-							'<img src="../img/t04.png" /><span class="item">登录密码</span>' +
+							'<span class="pc-icon security-lock-icon"></span>' +
+							'<span class="item">登录密码</span>' +
 							'<span class="text">安全级别：</span><span class="text security-level">中</span>' +
 							'<a class="change-login-pwd">修改</a>' +
+							'<span class="clear"></span>' +
 						'</div>' +
 
 						'<div>' +
-							'<div class="status warn"></div>' +
-							'<img src="../img/t05.png" /><span class="item">资金密码</span>' +
+							'<span class="pc-icon security-key-icon"></span>' +
+							'<span class="item">资金密码</span>' +
 							'<span class="text">安全级别：</span><span class="text security-level">中</span>' +
 							'<a class="change-withdraw-pwd">设置</a>' +
 						'</div>' +
 
 						'<div class="row3">' +
-							'<div class="status pass"></div>' +
-							'<img src="../img/t06.png" /><span class="item">银行卡</span>' +
+							'<span class="pc-icon security-card-icon"></span>' +
+							'<span class="item">银行卡</span>' +
 							'<span class="text">已绑定<span class="card-count">--</span>张</span>' +
 							'<a class="card-manage">管理</a>' +
 						'</div>' +
 
-						// '<div class="row4">' +
-						// 	'<div class="status pass"></div>' +
-						// 	'<img src="../img/t07.png" /><span class="item">手机号码</span>' +
-						// 	'<span class="text">128******63</span>' +
-						// 	'<a class="set-phonenumber">修改</a>' +
-						// '</div>' +
+						'<div class="row4">' +
+							'<span class="pc-icon security-phone-icon"></span>' +
+							'<span class="item">手机号码</span>' +
+							'<span class="text">128******63</span>' +
+							'<a class="set-phonenumber">修改</a>' +
+						'</div>' +
 
-						// '<div class="row5">' +
-						// 	'<div class="status pass"></div>' +
-						// 	'<img src="../img/t08.png" /><span class="item">电子邮箱</span>' +
-						// 	'<span class="text">立即绑定</span>' +
-						// 	'<a class="set-email">修改</a>' +
-						// '</div>' +
+						'<div class="row5">' +
+							'<span class="pc-icon security-mail-icon"></span>' +
+							'<span class="item">电子邮箱</span>' +
+							'<span class="text">立即绑定</span>' +
+							'<a class="set-email">修改</a>' +
+						'</div>' +
 
 					'</div>' +								
 				'</div>';
