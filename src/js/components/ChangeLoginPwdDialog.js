d7f38acd0cfe5c783ab58f5095b1ff0b11f7da07
mirek <mirek@eveb.com>
2016-10-28 11:10:38 +0800

    ~~

--- src/js/components/ChangeLoginPwdDialog.js
+++ src/js/components/ChangeLoginPwdDialog.js
@@ -6,32 +6,60 @@ $(function(){
 
 	ChangeLoginPwdDialog.prototype = new IMDialog();
 
-	ChangeLoginPwdDialog.prototype.initDom = function(){
+	ChangeLoginPwdDialog.prototype.initDom = function() {
+		var inputWidth  = 280;
+		var inputHeihgt = 30;
+
+		this.oldPasswordInput = new Input({
+			id: 'clp-old-password-input',
+			width: inputWidth,
+			height: inputHeihgt,
+			reg: app.passwordReg,
+			placeholder: '请输入您的旧密码',
+			type: 'password'
+		});
+
+		this.newPasswordInput = new Input({
+			id: 'clp-new-password-input',
+			width: inputWidth,
+			height: inputHeihgt,
+			reg: app.passwordReg,
+			placeholder: '请输入您的新密码',
+			type: 'password'
+		});
+
+		this.verifyPasswordInput = new Input({
+			id: 'clp-verify-password-input',
+			width: inputWidth,
+			height: inputHeihgt,
+			reg: app.passwordReg,
+			placeholder: '请再次输入您的新密码',
+			type: 'password'
+		});
+
 		var temp = '<div class="change-login-pwd-dialog">' +
 						'<div class="dialog-wrapper">' +
 							'<div class="dialog">' +
-								'<div class="row1">' +
-									'<div class="input-outer">' +
-										'<img src="../img/t04.png" />' +
-										'<input type="password" class="old-psw" placeholder="请输入您的旧密码" />' +
-									'</div>' +
+								'<div class="row0">' +
+									'修改密码' +
 								'</div>' +
 
-								'<div class="row2">' +
-									'<div class="input-outer">' +
-										'<img src="../img/t04.png" />' +
-										'<input type="password" class="new-psw" placeholder="请输入您的新密码" />' +
-									'</div>' +
+								'<div class="row">' +
+									'<span class="title">旧密码</span>' +
+									this.oldPasswordInput.getDom() +
 								'</div>' +
 
-								'<div class="row3">' +
-									'<div class="input-outer">' +
-										'<img src="../img/t04.png" />' +
-										'<input type="password" class="repeat-psw" placeholder="请再次输入您的新密码" />' +
-									'</div>' +
+								'<div class="row">' +
+									'<span class="title">新密码</span>' +
+									this.newPasswordInput.getDom() +
 								'</div>' +
 
-								'<div class="row4">' +
+								'<div class="row">' +
+									'<span class="title">新密码</span>' +
+									this.verifyPasswordInput.getDom() +
+								'</div>' +
+
+								'<div class="btn-row">' +
 									'<div class="button ok">' +
 										'确定' +
 									'</div>' +
@@ -46,27 +74,36 @@ $(function(){
 					'<div class="overlay overlay5"></div>';
 
 		this.el = temp;
-	}
+	};
 
 	ChangeLoginPwdDialog.prototype.getDom = function(){
 		return this.el;
-	}
+	};
 
 	ChangeLoginPwdDialog.prototype.show = function(){
+		this.resetDialog();
 		this.showOverlay();
-	}
+	};
 
 	ChangeLoginPwdDialog.prototype.hide = function(){
 		this.hideOverlay();
-	}
+	};
+
+	ChangeLoginPwdDialog.prototype.resetDialog = function() {
+		this.allPass = false;
+		this.oldPasswordInput.setValue('');
+		this.newPasswordInput.setValue('');
+		this.verifyPasswordInput.setValue('');
+		this.zone.find('.ok, .cancel').removeClass('active');
+	};
 
 	ChangeLoginPwdDialog.prototype.commit = function() {
 		var callback;
 		var opt;
 		var that   = this;
-		var oValue = $.trim(this.oldPasswordInput.val());
-		var nValue = $.trim(this.newPasswordInput.val());
-		var rValue = $.trim(this.repeatPasswordInput.val());
+		var oValue = $.trim(this.oldPasswordInput.getValue());
+		var nValue = $.trim(this.newPasswordInput.getValue());
+		var rValue = $.trim(this.verifyPasswordInput.getValue());
 
 		if (nValue !== rValue) {
 			alert('新密码和确认密码不一样!');
@@ -99,35 +136,41 @@ $(function(){
 		};
 
 		Service.post(opt, callback);
-	}
+	};
+
+	ChangeLoginPwdDialog.prototype.checkInputPass = function () {
+		if (this.oldPasswordInput.isPass() && 
+			this.newPasswordInput.isPass() &&
+			this.verifyPasswordInput.isPass()) {
+
+			this.zone.find('.ok, .cancel').addClass('active');
+			this.allPass = true;
+		} else {
+			this.zone.find('.ok, .cancel').removeClass('active');
+			this.allPass = false;
+		}
+	};
 
 	ChangeLoginPwdDialog.prototype.bindEvents = function() {
-		var oldPasswordReg     =  app.passwordReg;
-		var newPasswordReg     =  app.passwordReg;
-		var repeatPasswordReg  =  app.passwordReg;
-		var inputEvents        =  'input';
-		var that               =  this;
+		var that = this;
 
-		this.oldPasswordPass    =  false;
-		this.newPasswordPass    =  false;
-		this.repeatPasswordPass =  false;
-		this.allPass            =  false;
-
-		this.zone                = $('.change-login-pwd-dialog');
-		this.oldPasswordInput    = this.zone.find('.row1 input:password');
-		this.newPasswordInput    = this.zone.find('.row2 input:password');
-		this.repeatPasswordInput = this.zone.find('.row3 input:password');
+		this.zone = $('.change-login-pwd-dialog');
 
 		this.zone.find('.ok').click(function () {
-			that.commit();
+			if ($(this).hasClass('active')) {
+				that.commit();
+			}
 		});
 
 		this.zone.find('.cancel').click(function () {
 			that.hide();
 		});
 
+		this.oldPasswordInput.bindEvents(this.checkInputPass.bind(this));
+		this.newPasswordInput.bindEvents(this.checkInputPass.bind(this));
+		this.verifyPasswordInput.bindEvents(this.checkInputPass.bind(this));
         this.bindOverlayEvents();
-	}
+	};
 
 	window.ChangeLoginPwdDialog = ChangeLoginPwdDialog;
 
