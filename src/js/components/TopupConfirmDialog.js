769c0725ef7b2964815fd108af990d4658cfe10d
mirek <mirek@eveb.com>
2016-10-31 13:22:04 +0800

    ~~

--- src/js/components/TopupConfirmDialog.js
+++ src/js/components/TopupConfirmDialog.js
@@ -7,7 +7,18 @@
 
 	TopupConfirmDialog.prototype = new IMDialog();
 
-	TopupConfirmDialog.prototype.initDom = function(){
+	TopupConfirmDialog.prototype.initDom = function() {
+		var ms;
+		var date  = new Date();
+		var nowMs = Date.now();
+
+		ms   = nowMs + 20 * 60 * 1000;
+		date.setTime(ms);
+
+		date = 	date.getHours() + ':' + 
+				date.getMinutes() + ':' + 
+				date.getSeconds();
+
 		this.button = new Button({
 			id: 'topup-confirm-dialog-button',
 			name: '登录网上银行充值',
@@ -18,82 +29,79 @@
 		var temp = '<div class="topup-confirm-dialog">' +
 						'<div class="dialog-wrapper">' +
 							'<div class="dialog">' +
-								'<div class="row1">' +
-									'<div class="title">充值确认</div>' +
-									'<div class="close">×</div>' +
-									'<div class="clear"></div>' +
+								'<div class="title">' +
+									'<span>充值确认</span>' +
+									'<span class="close">×</span>' +
 								'</div>' +
 
-								'<div class="row2">' +
-									'<table>' +
-										'<tbody>' +
-											'<tr>' +
-												'<td class="name bank">充值银行：</td>' +
-												'<td class="value bank-value"></td>' +
-											'</tr>' +
-											'<tr>' +
-												'<td class="name amount">充值金额：</td>' +
-												'<td class="value amount-value"></td>' +
-											'</tr>' +
-											'<tr>' +
-												'<td class="name bookid">订单编号：</td>' +
-												'<td class="value sncode-value">SN00204040420332</td>' +
-											'</tr>' +
-										'</tbody>' +
-									'</table>' +
+								'<div class="content">' +
+									'<div class="row2">' +
+										'<div>' +
+											'<span class="name bank">充值银行：</span>' +
+											'<span class="value bank-value"></span>' +
+										'</div>' +
+										'<div>' +
+											'<span class="name amount">充值金额：</span>' +
+											'<span class="value amount-value"></span>' +
+										'</div>' +
+										'<div>' +
+											'<span class="name bookid">订单编号：</span>' +
+											'<span class="value sncode-value">SN00204040420332</span>' +
+										'</div>' +
+									'</div>' +
+
+									'<div class="row3">' +
+										'<span class="dot"></span>' +
+										'<span class="countdown-info">为保证充值成功，请在15分钟之内完成支付。截止时间：</span>' +
+										'<span class="countdown-time">' + date +'</span>' +
+									'</div>' +
+
+									'<div class="row4">' +
+										'<table cellpadding="0" cellspacing="0">' +
+											'<tbody>' +
+												'<tr>' +
+													'<td colspan="2">收款方信息确认</td>' +
+												'</tr>' +
+												'<tr>' +
+													'<td class="name">收款银行</td>' +
+													'<td class="value bank-name-value"></td>' +
+												'</tr>' +
+												'<tr>' +
+													'<td class="name">账户名</td>' +
+													'<td class="value">' +
+														'<div class="left user-name-value">刘文慧</div>' +
+														'<div class="right copy" data-clipboard-target=".user-name-value">复制</div>' +
+														'<div class="clear"></div>' +
+													'</td>' +
+												'</tr>' +
+												'<tr>' +
+													'<td class="name">收款账号</td>' +
+													'<td class="value">' +
+														'<div class="left account-value"></div>' +
+														'<div class="right copy" data-clipboard-target=".account-value">复制</div>' +
+														'<div class="clear"></div>' +
+													'</td>' +
+												'</tr>' +
+												'<tr>' +
+													'<td class="name">附言</td>' +
+													'<td class="value">' +
+														'<div class="left postscript"></div>' +
+														'<div class="right copy" data-clipboard-target=".postscript">复制</div>' +
+														'<div class="clear"></div>' +
+													'</td>' +
+												'</tr>' +
+											'</tbody>' +
+										'</table>' +
+									'</div>' +
+
+									'<p class="row5">' +
+										'注：附言在部分网站上会以"备注"，"用途"等名词出现，请务必正确填写此项信息，填写错误或不填写会影响充值到账' +
+									'</p>' +
+
+									// '<div class="row5">' +
+									// 	this.button.getDom() +
+									// '</div>' +
 								'</div>' +
-
-								'<div class="row3">' +
-									'<span class="dot"></span>' +
-									'<span class="countdown-info">为保证充值成功，请在15分钟之内完成支付。截止时间：</span>' +
-									'<span class="countdown-time">10:26:30</span>' +
-								'</div>' +
-
-								'<div class="row4">' +
-									'<table>' +
-										'<tbody>' +
-											'<tr>' +
-												'<td colspan="2">收款方信息确认</td>' +
-											'</tr>' +
-											'<tr>' +
-												'<td class="name">收款银行</td>' +
-												'<td class="value bank-name-value"></td>' +
-											'</tr>' +
-											'<tr>' +
-												'<td class="name">账户名</td>' +
-												'<td class="value">' +
-													'<div class="left user-name-value">刘文慧</div>' +
-													'<div class="right copy" data-clipboard-target=".user-name-value">复制</div>' +
-													'<div class="clear"></div>' +
-												'</td>' +
-											'</tr>' +
-											'<tr>' +
-												'<td class="name">收款账号</td>' +
-												'<td class="value">' +
-													'<div class="left account-value"></div>' +
-													'<div class="right copy" data-clipboard-target=".account-value">复制</div>' +
-													'<div class="clear"></div>' +
-												'</td>' +
-											'</tr>' +
-											'<tr>' +
-												'<td class="name">附言</td>' +
-												'<td class="value">' +
-													'<div class="left postscript"></div>' +
-													'<div class="right copy" data-clipboard-target=".postscript">复制</div>' +
-													'<div class="clear"></div>' +
-												'</td>' +
-											'</tr>' +
-										'</tbody>' +
-									'</table>' +
-								'</div>' +
-
-								'<p class="row5">' +
-									'注：附言在部分网站上会以"备注"，"用途"等名词出现，请务必正确填写此项信息，填写错误或不填写会影响充值到账' +
-								'</p>' +
-
-								// '<div class="row5">' +
-								// 	this.button.getDom() +
-								// '</div>' +
 							'</div>' +
 						'</div>' +
 					'</div>' +
@@ -115,9 +123,9 @@
 			this.firstTime = true;
 		}
 
-        this.zone.find('.row2 table .bank-value').text(data.PayBankName);
-        this.zone.find('.row2 table .amount-value').text(data.Amount + '元');
-        this.zone.find('.row2 table .sncode-value').text(data.SNCode);
+        this.zone.find('.row2 .bank-value').text(data.PayBankName);
+        this.zone.find('.row2 .amount-value').text(data.Amount + '元');
+        this.zone.find('.row2 .sncode-value').text(data.SNCode);
 	}
 
 	TopupConfirmDialog.prototype.hide = function(){
@@ -142,9 +150,9 @@
             }
 
             that.userAdminBankData = data;
-            that.zone.find('.row3 table .bank-name-value').text(data[0].Bank.BankName);
-            that.zone.find('.row3 table .user-name-value').text(data[0].AccountName);
-            that.zone.find('.row3 table .account-value').text(data[0].AccountNo);
+            that.zone.find('.row4 table .bank-name-value').text(data[0].Bank.BankName);
+            that.zone.find('.row4 table .user-name-value').text(data[0].AccountName);
+            that.zone.find('.row4 table .account-value').text(data[0].AccountNo);
 		};
 
 		Service.get(opt, callback);
